255cfcd5483c3396711d5249663511c23c76a181
Pedro-Gagliardi <pedroegagliardi@gmail.com>
2022-05-13 20:38:28 -0300

    Entregable 3: bucles for y while

---
+++ factorial-for.js
@@ -0,0 +1,10 @@
+// Entregable 3 bucles for y while
+
+// - factorial-for.js -> Este archivo debe calcular el factorial de 10 utilizando un solo bucle for
+
+let factorial10 = 10;
+
+for (let i = 1; i < 10; i++) {
+    factorial10 = factorial10 * i;
+}
+console.log(factorial10);
---
+++ factorial-while.js
@@ -0,0 +1,13 @@
+// - factorial-while.js -> Este archivo debe calcular el factorial de 10 utilizando un bucle while
+
+let factorial10 = 10;
+let i = 1;
+
+while (i < 10) {
+    factorial10 = factorial10 * i;
+    i++;
+}
+console.log(factorial10);
+
+// El ejercicio con sentencia break lamentablemente no pude hacerlo. Luego de ver el video no logro 
+// aplicarlo al ejemplo de factorial
