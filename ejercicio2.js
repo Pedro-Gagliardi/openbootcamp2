e8eec991467bafb242c819a69a39958149cd1c22
Pedro-Gagliardi <pedroegagliardi@gmail.com>
2022-05-12 22:45:27 -0300

    Entregable 2 - Listas, objetos y fechas

--- ejercicio2.js
+++ ejercicio2.js
@@ -1,8 +1,16 @@
-var saludo = 'Hola mundo';
+// Ejercicio entregable "Listas, objetos y fechas"
 
-function saludar() {
-    var saludo ='Hello world';
-    console.log(saludo);
+
+const fechaLibroFav = new Date(1951, 6, 16);
+const miNacimiento = new Date(1991, 8, 30);
+const libroFavorito = {
+    titulo: "El guardián entre el centeno",
+    autor: "J. D. Salinger",
+    fecha: fechaLibroFav,
+    url: "https//www.librosdemario.com/el-guardian-entre-el-centeno-7-leer-online-gratis" // No sé si logré comprender la propiedad url, disculpas.
 }
 
-saludar(console.log(saludo));
+const misDatos = ["Pedro", 30, true, miNacimiento, libroFavorito];
+console.log(misDatos);
+
+
