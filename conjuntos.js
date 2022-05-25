1dc4e53e79d21d3582044b8b9e76a42388e1cef2
Pedro-Gagliardi <pedroegagliardi@gmail.com>
2022-05-25 00:00:44 -0300

    Ejercicio 10 - Set

---
+++ conjuntos.js
@@ -0,0 +1,8 @@
+// Ejecricio 10 - Set
+const arrayMiFamilia = ['Úrsula', 'Luis', 'Milagros', 'Tomás', 'Teresita', 'Rafael', 'Celeste', 'Pedro'];
+
+const setMiFamilia = new Set(arrayMiFamilia);
+
+setMiFamilia.add('Pedro');
+setMiFamilia.add('Javascript');
+console.log(setMiFamilia);
