7833b2aa5eaff2b032776e0c08f6f936e461c7bd
Pedro-Gagliardi <pedroegagliardi@gmail.com>
2022-08-29 22:47:17 -0300

    Ejercicio 13 - DOM y jQuery

---
+++ ejercicio13/index.js
@@ -0,0 +1,17 @@
+$(".alert").click(() => {
+    console.log("Hola, estoy utilizando jQuery")
+})
+
+
+
+// const click = document.getElementById("alert")
+
+// click.addEventListener("click", () => {
+//     alert("Click en el botón")
+// })
+
+// Jquery
+
+
+
+
